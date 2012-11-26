b35f69c984ee60b0d8dc494b3efb92221cb56bee
abu <abu@e-conomic.com>
2012-11-27 00:05:35 +0100

    abu3 - port changed in hope it helps heroku

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var app = require('express')();
-var port = process.env.PORT || 8090;
+var express = require('express');
+var app = express(express.logger());
 
-app.get('/', function(req, res, next){
+app.get('/', function(req, res){
 	var id = Math.floor((Math.random()*1000)+1);
 	var script = "(function(){var gotcha=document.cookie.indexOf('spooky-cookie-"+id+"')!==-1;spookyCookie(gotcha);})();";
 	res.cookie('spooky-cookie-'+id, 'love', { maxAge: 10000 });
 	res.send(script);
 });
 
-app.listen(port);
-console.log('listening on port: ' + port);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
